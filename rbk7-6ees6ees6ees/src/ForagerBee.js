5f33b5a7206bb1aabb6f06d56b3425428f3f4234
Anes <marzoukianas@gmail.com>
2019-12-04 15:33:05 +0100

    6ees

--- rbk7-6ees6ees6ees/src/ForagerBee.js
+++ rbk7-6ees6ees6ees/src/ForagerBee.js
@@ -1,3 +1,26 @@
-class ForagerBee {
-  // TODO..
+/*
+the Bee superclass
+ an age property that is set to 10
+ a job property that is set to find pollen
+ a color property inherited from bee that is set to yellow
+ a food property that is inherited from grub
+ an eat method that is inherited from grub
+ a canFly property that is set true
+ a treasureChest property that is set to an empty array []
+ a forage method that allows the bee to add a treasure to the treasureChest
+*/
+
+class ForagerBee extends Bee {
+  
+  constructor(age = 10, job = "find pollen", canFly = true) {
+  	super();
+  	this.age = age;
+  	this.job = job;
+  	this.canFly = canFly;
+  	this.treasureChest = [];
+  }
+
+  forage(treasure) {
+  	this.treasureChest.push(treasure);
+  }
 };
--- rbk7-6ees6ees6ees/src/RetiredForagerBee.js
+++ rbk7-6ees6ees6ees/src/RetiredForagerBee.js
@@ -1,3 +1,14 @@
-class RetiredForagerBee {
-  // TODO..
+
+
+class RetiredForagerBee extends ForagerBee {
+	constructor() {
+		super(40, "gamble", false);
+		this.color = "grey";
+	}
+	forage() {
+		return "I am too old, let me play cards instead";
+	}
+	gamble(treasure) {
+		this.treasureChest.push(treasure);
+	}
 };
